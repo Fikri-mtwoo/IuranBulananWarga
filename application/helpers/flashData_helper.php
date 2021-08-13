4e07624a3732aee4a4853e9530278fc1f06cdbbe
Fikri-mtwoo <ahmad.fikrinew2801@gmail.com>
2021-08-13 12:40:00 +0700

    merubah helpers flashData_helper file : helpers(flashData_helper)

--- application/helpers/flashData_helper.php
+++ application/helpers/flashData_helper.php
@@ -23,5 +23,66 @@ if(! defined('BASEPATH')) exit('No direct script access allowed');
             $angka = $ci->vms->getRow('tabletransaksi');
             return $hasil = 'Tran'.($angka+1);
         }
+        function tahun($tahun){
+            $ci = &get_instance();
+            $ci->load->model('vmsModel','vm');
+            $nama_tahun = $ci->vm->getSelectWhereData('NamaTahun','tabletahuniuran',['IdTahunIuran'=>$tahun]);
+            return $nama_tahun[0]['NamaTahun'];
+        }
+        function bulan($bulan){
+            switch ($bulan) {
+                case '1':
+                    return "Januari";
+                    break;
+                
+                case '2':
+                    return "Februari";
+                    break;
+                
+                case '3':
+                    return "Maret";
+                    break;
+                
+                case '4':
+                    return "April";
+                    break;
+                
+                case '5':
+                    return "Mei";
+                    break;
+                
+                case '6':
+                    return "Juni";
+                    break;
+                
+                case '7':
+                    return "Juli";
+                    break;
+                
+                case '8':
+                    return "Agustus";
+                    break;
+                
+                case '9':
+                    return "September";
+                    break;
+                
+                case '10':
+                    return "Oktober";
+                    break;
+                
+                case '11':
+                    return "November";
+                    break;
+                
+                case '12':
+                    return "Desember";
+                    break;
+                
+                default:
+                    return "";
+                    break;
+            }
+        }
         
     }
